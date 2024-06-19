21630a62860d865008fe64975cffe9b04a77b5a4
Gem Newman <spurll@gmail.com>
2024-06-18 22:54:08 -0500

    Upgrade to work with Python 3.8 and latest package versions

--- goodplays/forms.py
+++ goodplays/forms.py
@@ -1,37 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, BooleanField, HiddenField, PasswordField, \
-    DateField, SelectField, SelectMultipleField, TextAreaField
-from wtforms.fields.html5 import IntegerField
-from wtforms.validators import Required, Optional, Email, EqualTo, Length
+from wtforms import StringField, BooleanField, HiddenField, PasswordField, \
+    DateField, SelectField, SelectMultipleField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, Optional, Email, EqualTo, Length
 
+from goodplays import app
 from goodplays.models import Status, Platform
 
 
+with app.app_context():
+    available_platforms = [
+        (p.id, p.name) for p in Platform.query.order_by(Platform.name).all()
+    ]
+
+
 class LoginForm(FlaskForm):
-    username = TextField('Username:', validators=[Required()])
-    password = PasswordField('Password:', validators=[Required()])
+    username = StringField('Username:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
     remember = BooleanField('Remember Me', default=True)
 
 
 class SignupForm(FlaskForm):
-    email = TextField('Email:', validators=[Email()])
-    name = TextField('Display Name:', validators=[Required()])
-    username = TextField('Username:', validators=[Required()])
+    email = StringField('Email:', validators=[Email()])
+    name = StringField('Display Name:', validators=[DataRequired()])
+    username = StringField('Username:', validators=[DataRequired()])
     password = PasswordField('Password:', validators=[Length(min=16)])
     confirm = PasswordField('Confirm Password:', validators=[EqualTo('password')])
 
 
 class EditGameForm(FlaskForm):
-    name = TextField('Name', validators=[Required()])
-    image_url = TextField('Image URL')
-    gb_id = TextField('GB ID')
-    hltb_id = TextField('HLTB ID')
+    name = StringField('Name', validators=[DataRequired()])
+    image_url = StringField('Image URL')
+    gb_id = StringField('GB ID')
+    hltb_id = StringField('HLTB ID')
     description = TextAreaField('Description')
     released = DateField('Released', format='%Y-%m-%d',
         validators=[Optional()])
-    platforms = SelectMultipleField('Platforms', coerce=int, choices=[
-        (p.id, p.name) for p in Platform.query.order_by(Platform.name).all()
-    ])
+    platforms = SelectMultipleField('Platforms', coerce=int,
+        choices=available_platforms)
 
 
 class AddPlayForm(FlaskForm):
@@ -41,22 +46,22 @@ class AddPlayForm(FlaskForm):
         validators=[Optional()])
     status = SelectField('Status', id='add-status', coerce=Status.coerce,
         choices=Status.choices())
-    tags = TextField('Tags', id='add-tags')
-    comments = TextField('Comments', id='add-comments')
+    tags = StringField('Tags', id='add-tags')
+    comments = StringField('Comments', id='add-comments')
     rating = HiddenField('Rating', id='add-rating')
     fave = HiddenField('Fave', id='add-fave')
 
 
 class EditPlayForm(FlaskForm):
-    id = HiddenField('ID', id='edit-id', validators=[Required()])
+    id = HiddenField('ID', id='edit-id', validators=[DataRequired()])
     started = DateField('Started', id='edit-started', format='%Y-%m-%d',
         validators=[Optional()])
     finished = DateField('Finished', id='edit-finished', format='%Y-%m-%d',
         validators=[Optional()])
     status = SelectField('Status', id='edit-status', coerce=Status.coerce,
         choices=Status.choices())
-    tags = TextField('Tags', id='edit-tags')
-    comments = TextField('Comments', id='edit-comments')
+    tags = StringField('Tags', id='edit-tags')
+    comments = StringField('Comments', id='edit-comments')
     rating = HiddenField('Rating', id='edit-rating')
     fave = HiddenField('Fave', id='edit-fave')
 
