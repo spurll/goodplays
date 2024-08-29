74a06bed0f803ac113ca5722ab6c9a6a9c72fe46
Gem Newman <spurll@gmail.com>
2024-08-29 18:24:37 -0500

    Temp updates to fix HLTB integration

--- goodplays/hltb.py
+++ goodplays/hltb.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from time import sleep
 from difflib import SequenceMatcher
+from howlongtobeatpy import HowLongToBeat
 import requests, json, re
 
 
@@ -19,13 +20,16 @@ HEADERS = {
 
 
 class HLTB():
-    def __init__(self, data):
-        self.name = data.get('game_name')
-        self.id = data.get('game_id')
-        self.main = data.get('comp_main', 0) / 3600
-        self.extra = data.get('comp_plus', 0) / 3600
-        self.comp = data.get('comp_100', 0) / 3600
-        self.all = data.get('comp_all', 0) / 3600
+    def __init__(self, json_data=None, api_data=None):
+        if not json_data and api_data:
+            json_data = api_data.json_content
+
+        self.name = json_data.get('game_name')
+        self.id = json_data.get('game_id')
+        self.main = json_data.get('comp_main', 0) / 3600
+        self.extra = json_data.get('comp_plus', 0) / 3600
+        self.comp = json_data.get('comp_100', 0) / 3600
+        self.all = json_data.get('comp_all', 0) / 3600
         self.no_data = self.main + self.extra + self.comp + self.all == 0
 
     @property
@@ -56,7 +60,28 @@ def hours(i):
         ' hour' + ('' if i < 2 else 's')
 
 
+# Unfortunately HLTB has now added a randomly-generated API key to prevent
+# parsing; will now use howlongtobeatpy to fetch this data rather than
+# reinventing the wheel
+
+
 def search(name):
+    results = HowLongToBeat().search(name)
+
+    if results is not None and len(results) > 0:
+        data = max(results, key=lambda x: x.similarity)
+        return data and HLTB(api_data=data), ""
+
+
+def details(game_id):
+    data = HowLongToBeat().search_from_id(game_id)
+    return data and HLTB(api_data=data), ""
+
+
+###############################################################################
+
+
+def search_old(name):
     payload = {
         'searchType': 'games',
         'searchTerms': name.split(),
@@ -79,22 +104,9 @@ def search(name):
     return data and HLTB(game), ""
 
 
-# Unfortunately HLTB seems to have removed (or just moved) their game details
-# API, so this function no longer works; use the new details function instead
-def details_old(game_id):
-    url = DETAILS_URL.format(id=game_id)
-    r = requests.get(url, headers=HEADERS)
-
-    if r.status_code != 200:
-        return {}, f'Request to {url} returned {r.status_code}'
-
-    data = r.json().get('data', {}).get('game')
-    return data and HLTB(data[0]), ""
-
-
 # Though we can't get the JSON via the API anymore, that JSON is still supplied
 # to the main HTML page in props, and remains highly parsable!
-def details(game_id):
+def details_old(game_id):
     url = DETAILS_HTML_URL.format(id=game_id)
     r = requests.get(url, headers=HEADERS)
 
@@ -111,3 +123,15 @@ def details(game_id):
 
     return data and HLTB(data[0]), ""
 
+
+# Unfortunately HLTB seems to have removed (or just moved) their game details
+# API, so this function no longer works; use the new details function instead
+def details_very_old(game_id):
+    url = DETAILS_URL.format(id=game_id)
+    r = requests.get(url, headers=HEADERS)
+
+    if r.status_code != 200:
+        return {}, f'Request to {url} returned {r.status_code}'
+
+    data = r.json().get('data', {}).get('game')
+    return data and HLTB(data[0]), ""
